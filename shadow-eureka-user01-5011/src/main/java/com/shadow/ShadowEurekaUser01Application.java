cfb985f13cf5f93da001495b06b38cd6cccf73a9
yangwangv5876 <yangwangv5876@163.com>
2019-06-18 18:01:00 +0800

    ribbon+feign集群

--- shadow-eureka-user01-5011/src/main/java/com/shadow/ShadowEurekaUser01Application.java
+++ shadow-eureka-user01-5011/src/main/java/com/shadow/ShadowEurekaUser01Application.java
@@ -2,11 +2,13 @@ package com.shadow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Shadow
  * @date 2019/6/17 12:01
  */
+@EnableFeignClients
 @SpringBootApplication
 public class ShadowEurekaUser01Application {
 
