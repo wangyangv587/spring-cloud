dff36b315fd1c09cda7e418a3a8224e658584974
yangwangv5876 <yangwangv5876@163.com>
2019-06-18 15:32:15 +0800

    删除无用文件

---
+++ shadow-eureka-user01-5011/src/main/java/com/shadow/config/CommonConfig.java
@@ -0,0 +1,20 @@
+package com.shadow.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Shadow
+ * @date 2019/6/17 15:44
+ */
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
